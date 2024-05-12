df63a12c1fe2a5a527055b1add397a84605c85d6
unknown <DoctorHays998@yandex.ru>
2024-05-13 02:01:05 +0300

    Add CTA, Reviews and My Tour sections

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
 // import cls from '../public/index.scss'
+
+// const str = "Abcd алыр воро ddd"
+// const arr = [...str]
+// let tempString = ""
+// const regExp = /[а-я]/
+// let resArr = []
+// arr.forEach((item) => {})
+// arr.forEach((item) => {
+//     if(item.toLowerCase()){}
+// })
+// arr.forEach((item) => {
+//     if(regExp.test(item.toLowerCase())){
+//         tempString = [item, ...tempString]
+//     }
+//     else{
+//         resArr = [...resArr, ...tempString, item]
+//         tempString = ''
+//     }
+// })
+const compact = (arr) => {
+    const newArray = [...arr];
+    return newArray.filter((item) => item);
+}
+
+const doubleNumber = (arr) => {
+    return arr.map((item) => item*2)
+}
+
+const sort = (arr) => {
+    return [...arr].sort();
+}
+
+const compouse = (arr, ...args) => {
+    let newArray = [...arr];
+    [...args].forEach((fn) => {
+        newArray = fn(newArray);
+    })
+}
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = (env) => {
     return {
         mode: env.mode,
-        entry: path.resolve(__dirname, 'src', 'index.scss'),
+        entry: [path.resolve(__dirname, 'src', 'index.scss')],
         output: {
             path: path.resolve(__dirname, 'build'),
             filename: '[name].[contenthash].js',
@@ -24,11 +24,15 @@ module.exports = (env) => {
                 {
                     test: /\.(png|svg|jpg|jpeg|gif)$/i,
                     type: 'asset/resource',
-                }
+                },
+                {
+                    test: /\.html$/i,
+                    loader: "html-loader",
+                },
             ],
         },
         plugins: [
-            new HtmlWebpackPlugin({template: path.resolve(__dirname, 'src', 'public', 'index.html')}),
+            new HtmlWebpackPlugin({template: path.resolve(__dirname, 'src', 'public', 'index.html'), favicon: path.resolve(__dirname, 'src', 'assets', 'Images', 'favicon.png')}),
             new MiniCssExtractPlugin()
         ],
         devServer: {
